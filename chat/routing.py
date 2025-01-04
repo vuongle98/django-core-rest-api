ca9be8b2869634beea7f14bfc839739a76d1f7a9
Vuong Le <vvuong1998@gmail.com>
2025-01-04 14:44:13 +0700

    fix: fix socket path, chat UI

--- chat/routing.py
+++ chat/routing.py
@@ -2,6 +2,5 @@ from . import consumer
 from django.urls.conf import path
 
 websocket_urlpatterns = [
-    path('ws/chat/<str:room_name>/', consumer.ChatConsumer.as_asgi()),
-    # path('ws/personal_chat/<str:room_name>/', consumer.PersonalConsumer.as_asgi()),
+    path('/chat/<str:room_name>/', consumer.ChatConsumer.as_asgi())
 ]
